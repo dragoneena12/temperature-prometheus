cb1df3d6fecf250e28323a8a4e089c9b773532c0
lapi <tyoutugai.7@gmail.com>
2020-05-01 17:08:59 +0900

    textfile

--- config/python/app/app.py
+++ config/python/app/app.py
@@ -1,41 +1,23 @@
-from flask import Flask
-from werkzeug.middleware.dispatcher import DispatcherMiddleware
-from werkzeug.serving import run_simple
-from prometheus_client import CollectorRegistry, make_wsgi_app, Gauge
+from prometheus_client import Gauge, write_to_textfile, REGISTRY
 import RPi.GPIO as GPIO
 import dht11
 import time
-from datetime import datetime
 
-# initialize GPIO
+TMP_PATH = '/textfile/dht11.prom'
+
 GPIO.setwarnings(False)
 GPIO.setmode(GPIO.BCM)
 GPIO.cleanup()
 
 module = dht11.DHT11(pin=24)
 
-registry = CollectorRegistry()
-g = Gauge('dht11', 'dht11', ['kind'], registry=registry)
+g1 = Gauge('temperature', 'Gauge')
+g2 = Gauge('humidity', 'Gauge')
 
-app = Flask(__name__)
-
-@app.route('/')
-def index():
-    return 'index'
-
-@app.before_request
-def before_request():
-    while True:
-        result = module.read()
-        if result.is_valid():
-            g.labels('temperature').set(result.temperature)
-            g.labels('humidity').set(result.humidity)
-            break
-        time.sleep(1)
-
-app_dispatch = DispatcherMiddleware(app, {
-    '/metrics': make_wsgi_app()
-})
-
-if __name__ == '__main__':
-    run_simple('172.24.0.4', 8000, app_dispatch, use_reloader=True, use_debugger=True, use_evalex=True)
+while True:
+    result = module.read()
+    if result.is_valid():
+        g1.set(result.temperature)
+        g2.set(result.humidity)
+        write_to_textfile(TMP_PATH, REGISTRY)
+    time.sleep(1)
